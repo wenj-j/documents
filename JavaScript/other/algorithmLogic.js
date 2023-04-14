9120a2bb40a67490e3f70b8b587e8eff66df736f
wenj-j <121803110+wenj-j@users.noreply.github.com>
2023-04-14 11:04:07 +0900

    Update algorithmLogic.js

--- JavaScript/other/algorithmLogic.js
+++ JavaScript/other/algorithmLogic.js
@@ -1,3 +1,18 @@
+
+
+//버블 정렬
+function sort(line){
+    let swap;
+    for(let i=0;i<line.length;i++){
+        for(let j=0;j<line.length-1;j++){
+            if(!compareMin(line[j],line[j+1])){
+                swap = line[j];
+                line[j] = line[j+1];
+                line[j+1] = swap;
+            }
+        }
+    }    
+}
 //시간 비교
 console.log(compare("10:31","17:11"));
 
